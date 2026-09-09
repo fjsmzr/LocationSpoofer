d054107863f5debf8f1ba36934022d3e1e5f351c
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2026-09-09 09:14:16 +0800

    tmp: 发布正式版

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.suseoaa.locationspoofer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 20612
-        versionName = "2.6.13-beta-5"
+        versionCode = 20613
+        versionName = "2.6.13"
 
         vectorDrawables {
             useSupportLibrary = true
